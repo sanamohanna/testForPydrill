05e78f02635f6b6ebfbab7618bd4325da4576645
sanamohanna <sanamohanna2305@gmail.com>
2023-08-06 10:42:42 +0300

    Create aneelSana.java

---
+++ test/src/test/aneelSana.java
@@ -0,0 +1,18 @@
+package test;
+
+public class aneelSana {
+protected String sana;
+private String aneel;
+public aneelSana(String sana, String aneel) {
+	super();
+	this.sana = sana;
+	this.aneel = aneel;
+}
+public String getAneel() {
+	return aneel;
+}
+public void setAneel(String aneel) {
+	this.aneel = aneel;
+}
+
+}
